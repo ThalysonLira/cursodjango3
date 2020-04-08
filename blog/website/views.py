a39e3d9ddca19cf1076573db68165d434e6279d5
Thalyson Lira <thalysonliratl.ln@gmail.com>
2020-04-08 15:07:16 -0300

    Adicionado sistema de template

--- blog/website/views.py
+++ blog/website/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def hello_blog(request):
-    return render(request, 'index.html')
+    tecnologias = ['Django', 'Python', 'Git', 'Html', 'Banco de Dados',
+                   'Linux', 'Nginx','Uwsgi','Systemclt']
+
+    data = {'name': 'Curso Django 3', 'lista': tecnologias}
+    return render(request, 'index.html', data)
