b77495da8c6523dad5be2d2c25d47379c99c01d6
Thalyson Lira <thalysonliratl.ln@gmail.com>
2020-04-08 19:13:17 -0300

    Modificação do admin e categorização de conteúdo

--- blog/website/admin.py
+++ blog/website/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post
 
-admin.site.register(Post)
+# Modifica a forma como vão ser exibidos os posts no admin
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'sub_title', 'categories']
+    search_fields = ['title', 'sub_title', 'categories']
+
+admin.site.register(Post, PostAdmin)
--- blog/website/models.py
+++ blog/website/models.py
@@ -1,7 +1,24 @@
 from django.db import models
 
 # Create your models here.
+# Categorizacao de conteudo
+class Categorias(models.TextChoices):
+    TECH = 'TC', 'Tecnologia'
+    CR = 'CR', 'Curiosidades'
+    GR = 'GR', 'Geral'
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     sub_title = models.CharField(max_length=200)
     content = models.TextField()
+
+    # Define as categorias, tamanho maximo (so sera salvo as abreviacoes no bd) e default
+    categories = models.CharField(
+        max_length=2,
+        choices=Categorias.choices,
+        default=Categorias.GR,
+    )
+
+    # Exibe o título do post na tela de criar post (admin)
+    def __str__(self):
+        return self.title
