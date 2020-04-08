c218831c8acae3ce6bee5004f8e6873e89a7367b
Thalyson Lira <thalysonliratl.ln@gmail.com>
2020-04-08 15:21:23 -0300

    Template conectado ao banco de dados

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from .models import Post
+
 
 # Create your views here.
 def hello_blog(request):
     tecnologias = ['Django', 'Python', 'Git', 'Html', 'Banco de Dados',
                    'Linux', 'Nginx','Uwsgi','Systemclt']
+    
+    list_posts = Post.objects.all()
+    
+    data = {
+        'name': 'Curso Django 3',
+        'lista': tecnologias,
+        'posts': list_posts }
 
-    data = {'name': 'Curso Django 3', 'lista': tecnologias}
     return render(request, 'index.html', data)
