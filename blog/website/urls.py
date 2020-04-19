1e9670ff8c6f100ebb07d35f4028f255e06b6b22
Thalyson Lira <thalysonliratl.ln@gmail.com>
2020-04-19 18:02:47 -0300

    Adicionado tela para ler posts

--- blog/website/urls.py
+++ blog/website/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import hello_blog
+from .views import hello_blog, post_details
 
 urlpatterns = [
-    path('', hello_blog),
+    path('', hello_blog, name='home_blog'),
+    path('post/<int:id>/', post_details, name='post_detail'),
 ]
